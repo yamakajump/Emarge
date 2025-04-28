0f768f7d579eca8685269cce30afde034261836e
yamakajump <corentin.batard2003@gmail.com>
2025-04-28 21:03:49 +0200

    changement fonctionnel

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,12 @@ set -e
 
 cd /home/container
 
-export PATH=/opt/venv/bin:/home/container/.local/bin:$PATH
-export PYTHONPATH=/home/container/.local/lib/python3.12/site-packages:$PYTHONPATH
+# Ajoute ~/.local/bin au PATH pour les dépendances user
+export PATH=$PATH:/home/container/.local/bin
 
+# Remplace les {{VAR}} par ${VAR} dans STARTUP pour compatibilité
 STARTUP_PROCESSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo "Starting bot with command: ${STARTUP_PROCESSED}"
 
+# Exécute la commande demandée par la plateforme
 eval "${STARTUP_PROCESSED}"
