1c68e4b17049ee1ce12523a06822af6c8a7b633e
yamakajump <corentin.batard2003@gmail.com>
2025-04-28 16:57:51 +0200

    suppression path

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,6 @@ set -e
 
 cd /home/container
 
-# Ajoute le vrai chemin Python du container Selenium
-export PATH="/opt/venv/bin:$PATH"
-export PATH=$PATH:/home/container/.local/bin
-
 # Remplace les {{VAR}} par ${VAR} dans STARTUP pour compatibilité
 STARTUP_PROCESSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo "Starting bot with command: ${STARTUP_PROCESSED}"
